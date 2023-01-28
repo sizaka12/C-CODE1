32607b6f11fa4a2faeffe4db57e64edbcdc75f5e
sizaka12 <123449992+sizaka12@users.noreply.github.com>
2023-01-28 09:52:03 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static System.Console;
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, Khadka!");
+Console.WriteLine("Version:7.0.2");
+Console.WriteLine("Version: {0}", Environment.Version.ToString()); 
+string text = System.IO.File.ReadAllText("text.txt"); 
+Console.WriteLine(text); 
+Console.WriteLine("Version:7.0.2");
+Console.WriteLine("Version: {0}", Environment.Version.ToString()); 
